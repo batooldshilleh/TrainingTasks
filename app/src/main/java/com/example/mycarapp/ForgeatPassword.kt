2fa3b9a4263974897d22526ca6ff078672919566
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-03-01 12:50:22 +0200

    verification Code ui

---
+++ app/src/main/java/com/example/mycarapp/ForgeatPassword.kt
@@ -0,0 +1,11 @@
+package com.example.mycarapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForgeatPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgeat_password)
+    }
+}
