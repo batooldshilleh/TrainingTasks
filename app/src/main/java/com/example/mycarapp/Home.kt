ce307ec3fdb0e45f683a1ca18d19ff7e1a09fd81
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-03-08 21:30:37 +0200

    login logic

--- app/src/main/java/com/example/mycarapp/Home.kt
+++ app/src/main/java/com/example/mycarapp/Home.kt
@@ -1,11 +1,26 @@
 package com.example.mycarapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.topAppBar)
+        toolbar.setNavigationOnClickListener {
+            logout()
+        }
+    }
+
+    private fun logout() {
+        val sharedPrefs = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+        val editor = sharedPrefs.edit()
+        editor.clear()
+        editor.apply()
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
