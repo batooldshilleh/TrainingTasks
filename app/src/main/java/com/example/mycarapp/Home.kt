43ebf864e673381300af76c0048f299651e301a6
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-03-09 18:55:29 +0200

    show user type
    send user type from login to home

--- app/src/main/java/com/example/mycarapp/Home.kt
+++ app/src/main/java/com/example/mycarapp/Home.kt
@@ -2,19 +2,34 @@ package com.example.mycarapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class Home : AppCompatActivity() {
+    private lateinit var typeTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        showData()
+        appBarListener()
+
+    }
+
+    fun showData(){
+        typeTextView = findViewById(R.id.tvType)
+
+        val userType = intent.getStringExtra("userType")
+        typeTextView.text = userType
+    }
+
+    fun appBarListener(){
         val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.topAppBar)
         toolbar.setNavigationOnClickListener {
             logout()
         }
     }
-
     private fun logout() {
         val sharedPrefs = getSharedPreferences("MyPrefs", MODE_PRIVATE)
         val editor = sharedPrefs.edit()
