591682c1e288798a1b7d0bdeca33e9eeb9391f2a
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-03-11 07:32:20 +0200

    fragment switch

---
+++ Fragment/app/src/main/java/com/example/fragmentapp/FirstFragment.kt
@@ -0,0 +1,13 @@
+package com.example.fragmentapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+
+}
---
+++ Fragment/app/src/main/java/com/example/fragmentapp/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.fragmentapp
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentContainerView
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btnFirstFragment: Button
+    private lateinit var btnSecondFragment: Button
+    private lateinit var  fragmentContener: FragmentContainerView
+
+    val frag1 = FirstFragment()
+    val frag2 = SecondFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        initViewss()
+        defaultبFragment()
+        setupClickListeners()
+
+        }
+
+    fun initViewss(){
+        btnFirstFragment = findViewById(R.id.btnFragment1)
+        btnSecondFragment = findViewById(R.id.btn2)
+        fragmentContener = findViewById(R.id.fcvFragment1)
+    }
+
+    fun defaultبFragment(){
+        supportFragmentManager.beginTransaction().apply {
+            replace(fragmentContener.id,frag1)
+            commit()
+        }
+    }
+
+    fun setupClickListeners(){
+        btnFirstFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+
+                replace(R.id.fcvFragment1, frag1)
+                addToBackStack("first_fragment")
+                setReorderingAllowed(true)
+                commit()
+
+            }
+        }
+
+        btnSecondFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fcvFragment1, frag2)
+                addToBackStack("second_fragment")
+                setReorderingAllowed(true)
+                commit()
+
+            }
+        }
+    }
+    }
