a4e7adad069cb7f4399934d34f2800dd486b063e
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2024-03-13 22:19:26 +0200

    login task

--- app/src/main/java/com/example/tasks/MainActivity.kt
+++ app/src/main/java/com/example/tasks/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnLinearLayout: Button
+    private lateinit var btnConstarintLayout: Button
+    private lateinit var btnLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,16 +24,27 @@ class MainActivity : AppCompatActivity() {
 
     fun setupView(){
         btnLinearLayout = findViewById(R.id.btnTask1)
+        btnConstarintLayout = findViewById(R.id.btnTask2)
+        btnLogin = findViewById(R.id.btnTask3)
     }
 
     fun setupListener(){
         btnLinearLayout.setOnClickListener {
             navigateToFirstTask()
         }
+        btnConstarintLayout.setOnClickListener {  }
+        btnLogin.setOnClickListener {
+            navigateToTheardTask()
+        }
     }
 
     fun navigateToFirstTask(){
         val intent = Intent(this@MainActivity, LinearLayout::class.java)
+        startActivity(intent)
+    }
+
+    fun navigateToTheardTask(){
+        val intent = Intent(this,Login::class.java)
         startActivity(intent)
     }
 }
